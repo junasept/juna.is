3202f91b6bb9e34e6bace121fac6dfab723c2528
Austin Clark <austinwillclark@pm.me>
2025-08-06 23:39:32 -0500

    Prepare site deploy

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,12 @@
 // @ts-check
 
 import netlify from "@astrojs/netlify";
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://juna.is",
 	adapter: netlify(),
+	integrations: [sitemap()],
 });
